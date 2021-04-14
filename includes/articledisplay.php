fa32b82b15fb3d4ccf4b68542d47101f3f2a0497
fb0801 <74499472+fb0801@users.noreply.github.com>
2021-04-14 20:53:23 +0100

    Add files via upload

---
+++ includes/articledisplay.php
@@ -0,0 +1,99 @@
+<?php
+require_once('dbutils.php');
+
+function JoatArticleDisplay(){
+$pdo=connect();
+$results = $pdo->query("SELECT joat_user.user_id,joat_user.user_uid,joat_content.contentTitle,joat_content.contentshortDescription,
+joat_content.contentDescription,joat_content.joat_image FROM joat_user INNER JOIN joat_content on joat_user.user_id=joat_content.user_id WHERE joat_content.joat_made_content='Article'",
+PDO::FETCH_ASSOC);
+
+
+
+foreach ($results as $row) {
+    print("<a href='#' data-toggle='modal' data-target='#myModal'><img src='joat_images/".$row['joat_image']."'height = '120'/></td></a>");
+
+  print("<a href='#' data-toggle='modal' data-target='#myModal'>".$row['contentTitle']."</a>");
+  print("<br>");
+  print($row['contentshortDescription']);
+  print("<br>");
+print("<br>");
+print("<br>");
+print("<br>");
+print("<div class='modal' id='myModal'>");
+
+    print("<div class='modal-dialog modal-dialog-scrollable'>");
+      print("<div class='modal-content'>");
+
+
+ print(" <div class='modal-content'>");
+ print(" <div class='modal-header'>");
+      print("<button type='button' class='close' data-dismiss='modal'>&times;</button>");
+      print("<h4 class='modal-title'>".$row['contentTitle']."</h4>");
+	  print($row['user_uid']);
+    print("</div>");
+    print("<div class='modal-body'>");
+    print("<img src='joat_images/".$row['joat_image']."' height = '120'/></td>");
+      print("<p>".$row['contentDescription']."</p>");
+    print("</div>");
+    print("<div class='modal-footer'>");
+      print("<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>");
+    print("</div>");
+  print("</div>");
+
+print("</div>");
+print("</div>");
+
+print("</div>");
+}
+
+
+}
+
+function JoatNewsDisplay(){
+$pdo=connect();
+$results = $pdo->query("SELECT joat_user.user_id,joat_user.user_uid,joat_content.contentTitle,joat_content.contentshortDescription,
+joat_content.contentDescription,joat_content.joat_image FROM joat_user INNER JOIN joat_content on joat_user.user_id=joat_content.user_id WHERE joat_content.joat_made_content='News'",
+PDO::FETCH_ASSOC);
+
+
+
+foreach ($results as $row) {
+    print("<a href='#' data-toggle='modal' data-target='#myModal'><img src='joat_images/".$row['joat_image']."'height = '120'/></td></a>");
+
+  print("<a href='#' data-toggle='modal' data-target='#myModal'>".$row['contentTitle']."</a>");
+  print("<br>");
+  print($row['contentshortDescription']);
+  print("<br>");
+print("<br>");
+print("<br>");
+print("<br>");
+print("<div class='modal' id='myModal'>");
+
+    print("<div class='modal-dialog modal-dialog-scrollable'>");
+      print("<div class='modal-content'>");
+
+
+ print(" <div class='modal-content'>");
+ print(" <div class='modal-header'>");
+      print("<button type='button' class='close' data-dismiss='modal'>&times;</button>");
+      print("<h4 class='modal-title'>".$row['contentTitle']."</h4>");
+	  print($row['user_uid']);
+    print("</div>");
+    print("<div class='modal-body'>");
+    print("<img src='joat_images/".$row['joat_image']."' height = '120'/></td>");
+      print("<p>".$row['contentDescription']."</p>");
+    print("</div>");
+    print("<div class='modal-footer'>");
+      print("<button type='button' class='btn btn-default' data-dismiss='modal'>Close</button>");
+    print("</div>");
+  print("</div>");
+
+print("</div>");
+print("</div>");
+
+print("</div>");
+}
+
+
+}
+ ?>
